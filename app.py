aac0c6d176f0ca6515493fb600945868fc24c1cf
bdkelmon <bdkelmon@gmail.com>
2025-07-12 18:55:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template, jsonify
+from flask import send_from_directory
+import shutil 
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -6,6 +8,14 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 def home():
     return render_template('editor.html')  # Your HTML UI
 
+@app.route('/reset_index', methods=['POST'])
+def reset_index():
+    try:
+        shutil.copyfile('index_template.html', 'index.html')
+        return jsonify(success=True)
+    except Exception as e:
+        return jsonify(success=False, error=str(e))
+        
 @app.route('/update_index', methods=['POST'])
 def update_index():
     data = request.get_json()
@@ -23,6 +33,7 @@ def update_index():
         """)
 
     return jsonify(success=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
