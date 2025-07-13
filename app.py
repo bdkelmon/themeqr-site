4cafa557ff0d19afd0b14648eecf001694906135
bdkelmon <bdkelmon@gmail.com>
2025-07-12 21:40:25 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, jsonify
 from flask import send_from_directory
-import shutil 
+import shutil
+import os
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -36,4 +37,6 @@ def update_index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
