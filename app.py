7c0218b64ffcb902693b2b2d1dfa76a8ed21e334
Bdkelmon <bdkelmon@gmail.com>
2025-08-10 22:01:07 -0400

    theme_applier2 test

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def manager():
         return redirect(url_for('index'))
     
     print(f"Assigned vault_id: {vault_id}")
-    return render_template('theme_applier.html',
+    return render_template('theme_applier2.html',
                           supabase_url=os.getenv('SUPABASE_URL'),
                           supabase_key=os.getenv('SUPABASE_KEY'),
                           user=user,
